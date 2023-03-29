99ca7a269fea10c8d6c82d988106e2a6966f425e
jayakrishnajay <jayakrishnajay7@gmail.com>
2023-03-29 15:33:50 +0530

    first commit

---
+++ python for u.py
@@ -0,0 +1,10 @@
+###Python mini project
+
+height=int(input())
+if height>6 :
+    if height==7 :
+        print('Take care of ur bones')
+    else:
+        print('Play volleyball')
+else :
+    print('Play Badminton')
